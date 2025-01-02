6018ee91d26497d1895438bf9d114e88e2eff5d6
pikta <pikta_ka@PiktaTOS.pikta>
2025-01-02 17:03:58 +0100

    set the sql injection project

---
+++ web-cases/sql-injection-case/go.mod
@@ -0,0 +1,14 @@
+module lablabee.com/cybersecurity-discovery1/sql-injection
+
+go 1.23.4
+
+require (
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/gorilla/sessions v1.4.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gorm.io/driver/sqlite v1.5.7 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
---
+++ web-cases/sql-injection-case/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"lablabee.com/cybersecurity-discovery1/sql-injection/database"
+	"lablabee.com/cybersecurity-discovery1/sql-injection/handlers"
+	"lablabee.com/cybersecurity-discovery1/sql-injection/models"
+)
+
+func main() {
+	database.Initialize()
+	models.Migrate()
+	http.HandleFunc("/login", handlers.LoginPage)
+	http.HandleFunc("/authenticate", handlers.HandleLogin)
+	http.HandleFunc("/home", handlers.HomePage)
+	models.InsertUser("pikta", "pikta")
+	http.ListenAndServe(":8080", nil)
+
+}
